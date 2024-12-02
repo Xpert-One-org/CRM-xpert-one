585c3af5606a3dee49c70c785f5dad8e5519d481
Jerance <michael.jerance@gmail.com>
2024-12-02 15:03:44 +0100

    ✨ feat(drag-and-drop): implement drag-and-drop feature for selection page with cards for each columns

--- app/(crm)/mission/selection/[slug]/_components/SelectionDragAndDropTable.tsx
+++ app/(crm)/mission/selection/[slug]/_components/SelectionDragAndDropTable.tsx
@@ -1,14 +1,32 @@
+import { DragDropContext } from '@hello-pangea/dnd';
 import { FilterButton } from '@/components/FilterButton';
 import React from 'react';
-import SelectionDragAndDropRow from './SelectionDragAndDropRow';
+import SelectionDragAndDropColumns from './SelectionDragAndDropColumns';
 
 export default function SelectionDragAndDropTable() {
+  const handleDragEnd = (result: any) => {
+    const { source, destination } = result;
+
+    if (!destination) return;
+
+    if (
+      source.droppableId === destination.droppableId &&
+      source.index === destination.index
+    ) {
+      return;
+    }
+
+    console.log(
+      `Moved from ${source.droppableId} to ${destination.droppableId}`
+    );
+  };
+
   return (
-    <>
+    <DragDropContext onDragEnd={handleDragEnd}>
       <div className="grid grid-cols-8 gap-3">
         <FilterButton placeholder="Postulant" />
         <FilterButton placeholder="52 matching" />
-        <FilterButton placeholder="A l’étude" filter={false} />
+        <FilterButton placeholder="A l'étude" filter={false} />
         <FilterButton placeholder="Non retenu par XPERT ONE" filter={false} />
         <FilterButton placeholder="En discussions" filter={false} />
         <FilterButton placeholder="XPERT proposés" filter={false} />
@@ -16,8 +34,8 @@ export default function SelectionDragAndDropTable() {
         <FilterButton placeholder="XPERT Validés" filter={false} />
       </div>
       <div className="grid h-full grid-cols-8 gap-3">
-        <SelectionDragAndDropRow />
+        <SelectionDragAndDropColumns />
       </div>
-    </>
+    </DragDropContext>
   );
 }
