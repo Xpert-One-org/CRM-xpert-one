53baf94d8780991b0ea13900e9e84253971fd962
Thomas Aubert <90507019+N0rooo@users.noreply.github.com>
2025-01-15 15:10:18 +0100

    :sparkles: feat(todo): adding notifications for new todo or todo done

--- src/hooks/useNotifications.ts
+++ src/hooks/useNotifications.ts
@@ -1,24 +1,34 @@
 import { notifPerPage } from '@/data/constant';
-import { getChatNotification } from '@functions/notification';
+import { deleteNotification, getNotifications } from '@functions/notification';
 import type { DBNotification } from '@/types/typesDb';
 import { create } from 'zustand';
 
 type State = {
-  chatNotifications: DBNotification[] | null;
+  notifications: DBNotification[] | null;
+  setNotifications: (notifications: DBNotification[]) => void;
   fetchNotifications: () => Promise<void>;
+  removeNotification: (id: number) => void;
   from: number;
   to: number;
 };
 
 const useNotifications = create<State>((set) => ({
-  chatNotifications: null,
+  notifications: null,
+  setNotifications: (notifications) => set({ notifications }),
   from: 0,
   to: notifPerPage,
   fetchNotifications: async () => {
     const to = useNotifications.getState().to;
     const from = useNotifications.getState().from;
-    const { data } = await getChatNotification({ from: from, to: to });
-    set({ chatNotifications: data ?? [] });
+    const { data } = await getNotifications({ from: from, to: to });
+    set({ notifications: data ?? [] });
+  },
+  removeNotification: async (id) => {
+    await deleteNotification(id);
+    set((state) => ({
+      notifications:
+        state.notifications?.filter((notif) => notif.id !== id) ?? [],
+    }));
   },
 }));
 
