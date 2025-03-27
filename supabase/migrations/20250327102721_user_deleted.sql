773660d6c10539ca8531f5190ed5084b1ad5aabf
Thomas Aubert <thomas.aubert@edu.devinci.fr>
2025-03-27 11:28:20 +0100

    :wrench: config(migration): add migration file user deletion

---
+++ supabase/migrations/20250327102721_user_deleted.sql
@@ -0,0 +1,7 @@
+alter table "public"."profile_deleted" add column "email" text;
+
+alter table "public"."profile_deleted" add column "firstname" text;
+
+alter table "public"."profile_deleted" add column "lastname" text;
+
+
