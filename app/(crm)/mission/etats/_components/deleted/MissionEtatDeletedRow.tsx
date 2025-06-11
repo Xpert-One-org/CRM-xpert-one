35fcd9d4fc5fca7b0d19ebd1001f7d985fe4efb8
Thomas Aubert <thomas.aubert@edu.devinci.fr>
2025-06-11 16:54:59 +0200

    :sparkles: feat(mission): can re open mission deleted

--- app/(crm)/mission/etats/_components/deleted/MissionEtatDeletedRow.tsx
+++ app/(crm)/mission/etats/_components/deleted/MissionEtatDeletedRow.tsx
@@ -1,4 +1,11 @@
+import Button from '@/components/Button';
 import { Box } from '@/components/ui/box';
+import {
+  Credenza,
+  CredenzaClose,
+  CredenzaContent,
+  CredenzaTrigger,
+} from '@/components/ui/credenza';
 import { empty } from '@/data/constant';
 import {
   jobTitleSelect,
@@ -10,7 +17,9 @@ import { formatDateHour } from '@/utils/formatDates';
 import { getLabel } from '@/utils/getLabel';
 import { EyeIcon } from 'lucide-react';
 import { useRouter } from 'next/navigation';
-import React from 'react';
+import React, { useState } from 'react';
+import Image from 'next/image';
+import { useMissionStore } from '@/store/mission';
 
 export default function MissionEtatDeletedRow({
   mission,
@@ -19,6 +28,7 @@ export default function MissionEtatDeletedRow({
 }) {
   const router = useRouter();
   const createdAt = formatDate(mission.created_at);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleRedirectFicheMission = (number: string) => {
     const formattedNumber = number.replaceAll(' ', '-');
@@ -27,6 +37,13 @@ export default function MissionEtatDeletedRow({
 
   const handleRedirectFournisseur = (fournisseurId: string) => {
     router.push(`/fournisseur?id=${fournisseurId}`);
+  };
+  const { updateMission } = useMissionStore();
+
+  const handleReOpen = async () => {
+    setIsLoading(true);
+    await updateMission(mission.id.toString(), 'open');
+    setIsLoading(false);
   };
 
   return (
@@ -67,6 +84,43 @@ export default function MissionEtatDeletedRow({
         {mission.detail_deletion}
         <></>
       </Box>
+      <Credenza>
+        <CredenzaTrigger asChild>
+          <Button>Réouvrir la mission</Button>
+        </CredenzaTrigger>
+        <CredenzaContent className="font-fira mx-4 max-w-[946px] overflow-hidden rounded-sm border-0 bg-white bg-opacity-70 p-0 backdrop-blur-sm">
+          <div className="relative h-[175px] w-full">
+            <Image
+              src="/static/background.jpg"
+              fill
+              objectFit="cover"
+              alt="confirm-popup"
+            />
+          </div>
+          <div className="flex flex-col gap-y-spaceContainer p-6">
+            <div className="text-xl font-semibold text-gray-800">
+              Êtes-vous sûr de vouloir réouvrir cette mission ?
+              <p className="text-base font-normal text-gray-600">
+                Cette action restaurera la mission dans l'état "Ouverte"
+              </p>
+            </div>
+
+            <div className="flex gap-x-spaceSmall self-end">
+              <CredenzaClose asChild>
+                <Button variant={'outline'}>Annuler</Button>
+              </CredenzaClose>
+
+              <Button
+                onClick={handleReOpen}
+                className="w-fit self-end px-spaceContainer"
+                variant={'primary'}
+              >
+                {isLoading ? 'Chargement...' : 'Réouvrir'}
+              </Button>
+            </div>
+          </div>
+        </CredenzaContent>
+      </Credenza>
     </>
   );
 }
