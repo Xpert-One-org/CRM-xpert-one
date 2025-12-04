5564b495373ee59a89b6c7e60500d565abe6725b
Thomas Aubert <thomas.aubert@edu.devinci.fr>
2025-12-04 14:55:15 +0100

    :camera_flash: fix(version): update nextjs version

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['wxjnrjakjwjhvsiwhelt.supabase.co', 'wxjnrjakjwjhvsiwhelt.supabase.in', 'gcybwruhwuiaecucsroi.supabase.in']
-
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'wxjnrjakjwjhvsiwhelt.supabase.co',
+        port: '',
+        pathname: '/**',
+        search: '',
+      },
+    ],
   },
 };
 
